a6e1c16624a0bac9a4b8172075a1b3d0a93e48c8
Abdulla Al-Ali <abdulla.alali@qu.edu.qa>
2016-09-27 20:59:15 +0300

    ImplicitIntents: Use marshmallow's new run-time permission request and update the targetSdk

--- IntentsImplicit/ImplicitIntents/app/src/main/java/qa/edu/qu/cse/cmps497/implicitintents/MainActivity.java
+++ IntentsImplicit/ImplicitIntents/app/src/main/java/qa/edu/qu/cse/cmps497/implicitintents/MainActivity.java
@@ -1,9 +1,13 @@
 package qa.edu.qu.cse.cmps497.implicitintents;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v13.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -11,6 +15,8 @@ import android.widget.EditText;
 public class MainActivity extends Activity {
 
     final static String TAG = "MainActivity";
+    final static int MY_REQUEST_CODE = 43;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +25,8 @@ public class MainActivity extends Activity {
     }
 
     public void callClicked(View view) {
-        EditText editText = (EditText)findViewById(R.id.editText);
-        String number = editText.getText().toString();
+        mEditText = (EditText)findViewById(R.id.editText);
+        String number = mEditText.getText().toString();
 
 
         //Implicit intent
@@ -28,6 +34,27 @@ public class MainActivity extends Activity {
         //of the phone number
         //Note: this Action requires a special permission. What happens if you don't have it?
 
+        //see if we have the permission (For Marshmallow and above)
+        //pay attention that CALL_PHONE is a dangerous permission,
+        //therefore we need to explicitly check.
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE);
+
+        if (permissionCheck== PackageManager.PERMISSION_GRANTED) {
+            //call the number if we are GRANTED the permission
+            callNumber(number);
+        } else {
+
+            //we don't have permission :~( let's request it
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.CALL_PHONE}, MY_REQUEST_CODE);
+
+        }
+
+
+    }
+
+    private void callNumber(String number) {
         Intent myIntent = new Intent();
         myIntent.setAction(Intent.ACTION_CALL);
         myIntent.setData(Uri.parse("tel:" + number));
@@ -46,9 +73,42 @@ public class MainActivity extends Activity {
         Intent myIntent = new Intent();
         myIntent.setAction("qa.edu.qu.cse.cmps497.implicitintentslave.ACTION_LAUNCH");
         if (myIntent.resolveActivity(getPackageManager())!=null) {
-            startActivity(myIntent);
+            try {
+                startActivity(myIntent);
+            } catch (SecurityException e) {
+                Log.e(TAG, "I do not have the permission to launch the activity");
+            }
         } else {
             Log.e(TAG, "no intent is found!");
+        }
+    }
+
+
+    /**
+     * User made a choice on the dialog of permission
+     * This method is automatically called by the Android framework
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        //see if this is in response to my request above
+        if (requestCode == MY_REQUEST_CODE) {
+
+            //if grantResult is empty, user canceled
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                //user accepted permission. Let's call
+                callNumber(mEditText.getText().toString());
+
+            } else {
+
+                //the user refused to grant us permission.
+                //either request again, or give up
+                Log.i(TAG, "I don't have permission to call");
+                callClicked(null);
+
+            }
         }
     }
 }
